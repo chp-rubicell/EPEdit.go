0f1e2fce661918022e3aeeff93d93d515a998c7d
Rubicell <chp.rubicell@gmail.com>
2026-06-15 00:17:37 +0900

    started work on ParseIDD()

--- idd.go
+++ idd.go
@@ -3,19 +3,22 @@ package epedit
 import (
 	"bufio"
 	"os"
+	"strings"
 )
+
+// * Field and class definition
 
 // IDD field definition (ex. Outside_Boundary_Condition)
 type FieldDef struct {
 	Name             string
 	Required         bool
 	Units            string
-	Default          string //TODO deal with type conversion later...
+	Default          string // TODO: deal with type conversion later...
 	Autosizable      bool
 	Autocalculatable bool
 	Type             string   // alpha, real, integer, choice, etc.
 	Choices          []string // possible values for "\type choice"
-	//TODO add more later
+	// TODO: add more later
 }
 
 // IDD \extensible field properties (used in ClassDef)
@@ -36,7 +39,7 @@ type ClassDef struct {
 // IDD object that contains all of the definitions
 type IDD struct {
 	Version string
-	Classes map[string]*ClassDef // Map for fast search (without capitalization)
+	Classes map[string]*ClassDef // map for fast search (without capitalization)
 }
 
 func NewIDD() *IDD {
@@ -44,6 +47,8 @@ func NewIDD() *IDD {
 		Classes: make(map[string]*ClassDef),
 	}
 }
+
+// * Parse IDD file into IDD struct
 
 // open IDD file in filepath and return pointer for parsed IDD struct
 func ParseIDD(filepath string) (*IDD, error) {
@@ -56,5 +61,62 @@ func ParseIDD(filepath string) (*IDD, error) {
 	idd := NewIDD()
 	scanner := bufio.NewScanner(file)
 
-	return idd, err
+	// state machine
+	var currentGroup string
+	var currentClass *ClassDef
+	var currentField *FieldDef
+
+	for scanner.Scan() {
+		line := scanner.Text()
+
+		// 1. remove comments and whitespaces
+		if idx := strings.Index(line, "!"); idx != -1 {
+			line = line[:idx]
+		}
+		line = strings.TrimSpace(line)
+		if line == "" {
+			continue
+		}
+
+		// 2. parse \group
+		if strings.HasPrefix(line, `\group`) {
+			currentGroup = strings.TrimSpace(strings.TrimPrefix(line, `\group`))
+			continue
+		}
+
+		// 3. parse metadata for class or field level
+		if strings.HasPrefix(line, `\`) {
+			if currentField != nil {
+				// currently parsing a field
+				parseFieldProperty(currentField, line)
+			} else if currentClass != nil {
+				// no current field, but parsing a class (ex. \extensible, \memo)
+				parseClassProperty(currentClass, line)
+			}
+			continue
+		}
+
+		// 4. start new class or field (ends with `,` or `;`)
+	}
+
+	return idd, scanner.Err()
+}
+
+// * Helper functions for parsing class and field property
+
+func parseClassProperty(class *ClassDef, line string) {
+	if strings.HasPrefix(line, `\extensible`) {
+		class.Extensible = &ExtensibleDef{}
+		// TODO: parse other info such as \extensible:#
+	}
+	// TODO: \memo, \min-fields, etc.
+}
+
+func parseFieldProperty(field *FieldDef, line string) {
+	if strings.HasPrefix(line, `\type`) {
+		field.Type = strings.TrimSpace(strings.TrimPrefix(line, `\type`))
+	} else if line == `\required-field` {
+		field.Required = true
+	}
+	// TODO: \default, \key, etc.
 }
